125c1e3cb74e15c140c0d1cb95ad832bc2764954
Arti Smarti <arturbosch@gmx.de>
2016-07-05 13:22:04 +0200

    Implement help separated for all modes - Fixes#6

---
+++ src/main/java/com/gitlab/artismarti/tinbo/common/ClearCommand.kt
@@ -0,0 +1,21 @@
+package com.gitlab.artismarti.tinbo.common
+
+import org.fusesource.jansi.Ansi.ansi
+import org.fusesource.jansi.AnsiConsole
+import org.springframework.shell.core.annotation.CliCommand
+import org.springframework.stereotype.Component
+
+/**
+ * @author artur
+ */
+@Component
+class ClearCommand : Command {
+
+	override val id: String = "share"
+
+	@CliCommand(value = *arrayOf("cls", "clear"), help = "Clears the console")
+	fun clear() {
+		AnsiConsole.out().print(ansi().eraseScreen().cursor(0, 0))
+	}
+
+}
--- src/main/java/com/gitlab/artismarti/tinbo/common/HelpCommand.kt
+++ src/main/java/com/gitlab/artismarti/tinbo/common/HelpCommand.kt
@@ -7,7 +7,6 @@ import org.springframework.context.ApplicationContextAware
 import org.springframework.shell.core.JLineShellComponent
 import org.springframework.shell.core.annotation.CliCommand
 import org.springframework.shell.core.annotation.CliOption
-import org.springframework.shell.support.util.StringUtils
 import org.springframework.stereotype.Component
 
 /**
@@ -23,30 +22,25 @@ class HelpCommand : Command, ApplicationContextAware {
 	@CliCommand(value = "help", help = "List all commands usage")
 	fun obtainHelp(
 			@CliOption(key = arrayOf("", "command"), optionContext = "disable-string-converter availableCommands", help = "Command name to provide help for")
-			buffer: String?) {
+			buffer: String?): String {
 
-		if (StringUtils.isEmpty(buffer)) {
-			val shell = ctx.getBean("shell", JLineShellComponent::class.java)
-			val parser = shell.simpleParser
-			val commands: List<Command> = parser.commandMarkers.asSequence()
-					.filter { it is Command }
-					.map { it as Command }
-					.toList()
+		val shell = ctx.getBean("shell", JLineShellComponent::class.java)
+		val parser = shell.simpleParser
 
-			val currentModeCommands = when (ModeAdvisor.getMode()) {
-				Mode.START -> commands.filter { it.id == "start" || it.id == "help" }
-				Mode.TIMER -> commands.filter { it.id == "time" || it.id == "edit" || it.id == "share" }
-				Mode.NOTES -> commands.filter { it.id == "note" || it.id == "edit" || it.id == "share" }
-				Mode.TASKS -> commands.filter { it.id == "task" || it.id == "edit" || it.id == "share" }
-				else -> emptyList()
-			}
+		val commands: List<Command> = parser.commandMarkers.asSequence()
+				.filter { it is Command }
+				.map { it as Command }
+				.toList()
 
-			println(commands)
-			println(currentModeCommands)
-//			parser
-//			parser.obtainHelp("ls add")
+		val currentModeCommands = when (ModeAdvisor.getMode()) {
+			Mode.START -> commands.filter { it.id == "start" || it.id == "help" }
+			Mode.TIMER -> commands.filter { it.id == "time" || it.id == "edit" || it.id == "share" }
+			Mode.NOTES -> commands.filter { it.id == "note" || it.id == "edit" || it.id == "share" }
+			Mode.TASKS -> commands.filter { it.id == "task" || it.id == "edit" || it.id == "share" }
+			else -> emptyList()
 		}
-		println("its meeee")
+
+		return HelpParser(currentModeCommands).obtainHelp(buffer)
 	}
 
 	override fun setApplicationContext(applicationContext: ApplicationContext) {
