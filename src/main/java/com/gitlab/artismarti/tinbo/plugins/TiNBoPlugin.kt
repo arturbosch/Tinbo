b069d5c2cefd4ef41ed670c3f28db23a11f03988
ArtiSmarti <arturbosch@gmx.de>
2016-08-04 00:18:12 +0200

    Add tinbo plugin interface

---
+++ src/main/java/com/gitlab/artismarti/tinbo/plugins/TiNBoPlugin.kt
@@ -0,0 +1,10 @@
+package com.gitlab.artismarti.tinbo.plugins
+
+import com.gitlab.artismarti.tinbo.common.Command
+import org.springframework.stereotype.Component
+
+/**
+ * @author artur
+ */
+@Component
+interface TiNBoPlugin : Command
