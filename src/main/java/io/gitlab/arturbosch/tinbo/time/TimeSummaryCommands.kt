a94c4722f7e1f1775b97c6a3c840f4e59d52af33
ArtiSmarti <arturbosch@gmx.de>
2016-10-27 15:42:16 +0200

    Introduce available indicator for week command - #74

--- src/main/java/io/gitlab/arturbosch/tinbo/time/TimeSummaryCommands.kt
+++ src/main/java/io/gitlab/arturbosch/tinbo/time/TimeSummaryCommands.kt
@@ -2,7 +2,9 @@ package io.gitlab.arturbosch.tinbo.time
 
 import io.gitlab.arturbosch.tinbo.api.Command
 import io.gitlab.arturbosch.tinbo.api.Summarizable
+import io.gitlab.arturbosch.tinbo.config.ModeAdvisor
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.shell.core.annotation.CliAvailabilityIndicator
 import org.springframework.shell.core.annotation.CliCommand
 import org.springframework.stereotype.Component
 
@@ -14,6 +16,11 @@ open class TimeSummaryCommands @Autowired constructor(val summaryExecutor: WeekS
 		Command, Summarizable {
 
 	override val id: String = "time"
+
+	@CliAvailabilityIndicator("week")
+	fun isAvailable(): Boolean {
+		return ModeAdvisor.isTimerMode()
+	}
 
 	override fun sum(categories: List<String>): String {
 		if (categories.isEmpty()) {
