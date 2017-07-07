8a4e74433127ddf93630729988e2e9373b0afa5d
Artur Bosch <arturbosch@gmx.de>
2017-07-07 10:41:31 +0200

    Use newest lloc version 1.5

--- tinbo-lloc/src/main/kotlin/io/gitlab/arturbosch/tinbo/lloc/LLOCPlugin.kt
+++ tinbo-lloc/src/main/kotlin/io/gitlab/arturbosch/tinbo/lloc/LLOCPlugin.kt
@@ -1,16 +1,15 @@
 package io.gitlab.arturbosch.tinbo.lloc
 
 import io.gitlab.arturbosch.loc.core.LOC
+import io.gitlab.arturbosch.loc.languages.Language
 import io.gitlab.arturbosch.loc.languages.LanguageStrategyFactory
 import io.gitlab.arturbosch.tinbo.api.Command
 import io.gitlab.arturbosch.tinbo.plugins.TinboContext
 import io.gitlab.arturbosch.tinbo.plugins.TinboPlugin
 import org.springframework.shell.core.annotation.CliCommand
 import org.springframework.shell.core.annotation.CliOption
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.io.File
 import java.util.HashMap
-import java.util.stream.Collectors
 
 /**
  * Implements a command to count logical lines of code for specified project path.
@@ -25,34 +24,34 @@ class LLOCPlugin : TinboPlugin, Command {
 		return listOf(this)
 	}
 
-	private val languages = LanguageStrategyFactory.languages
-			.map { LanguageStrategyFactory.getInstance(it) }
-	private val nullStrategy = LanguageStrategyFactory.getInstance("")
+	private val languages = LanguageStrategyFactory.LANGUAGES.map { it.strategy }
+	private val nullStrategy = Language.UNSUPPORTED.strategy
 
 	@CliCommand("plugin lloc", help = "Runs lloc over specified project path and return the logical lines of code.")
-	fun run(@CliOption(key = arrayOf("")) path: String?): String {
-		return path?.let {
+	fun run(@CliOption(key = arrayOf("")) path: String?) = path?.let {
 
-			val project = Paths.get(path)
-			if (Files.notExists(project)) return "Specified path does not exist!"
+		val project = File(path)
+		if (!project.exists()) return "Specified path does not exist!"
 
-			val languageLocPairs = Files.walk(project)
-					.filter { Files.isRegularFile(it) }
-					.map { path ->
-						val strategy = languages.find {
-							it.isLangOfFileSame(path.toString())
-						} ?: nullStrategy
-						strategy.ending to LOC.count(strategy, path.toFile())
-					}.collect(Collectors.toList<Pair<String, Int>>())
+		val languageLocPairs = project.walkTopDown()
+				.filter { it.isFile }
+				.map { filePath ->
+					val strategy = languages.find {
+						it.isLangOfFileSame(filePath.toString())
+					} ?: nullStrategy
+					strategy.language.ending to LOC.count(strategy, filePath)
+				}
 
-			val languagePerLOC = languageLocPairs
-					.groupByTo(HashMap(), { it.first }, { it.second })
-					.map { it.key to it.value.sum() }
+		val languagePerLOC = languageLocPairs
+				.groupByTo(HashMap(), { it.first }, { it.second })
+				.map { it.key to it.value.sum() }
 
-			"Project: $path:\n" + languagePerLOC.filterNot { it.first == "null" }
-					.joinToString(separator = "") { "\t${it.first} - ${it.second}\n" } +
-					"\nTotal LOC: ${languagePerLOC.sumBy { it.second }}"
+		val languageToLocString = languagePerLOC
+				.filterNot { it.first == "NULL" }
+				.filterNot { it.second == 0 }
+				.joinToString(separator = "") { "\t${it.first} - ${it.second}\n" }
 
-		} ?: "You have to specify a project path to count LOC."
-	}
+		"Project: $path:\n" + languageToLocString + "\nTotal LOC: ${languagePerLOC.sumBy { it.second }}"
+
+	} ?: "You have to specify a project path to count LOC."
 }
