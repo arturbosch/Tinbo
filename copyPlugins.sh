46368835cbfa7b33e09eb8e770ec5141aeb3a165
Artur Bosch <arturbosch@gmx.de>
2018-04-17 06:34:10 +0200

    Create parent directories too

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -3,7 +3,7 @@ echo "Building projects and plugins"
 ./gradlew build
 echo "Deleting old plugins"
 rm -R ~/Tinbo/plugins/
-mkdir ~/Tinbo/plugins/
+mkdir -p ~/Tinbo/plugins/
 find $PWD/tinbo-*/build/libs/ -type f | awk '!/tinbo-plugin-api/ && !/tinbo-platform/' | while read file ; do
 cp ${file} ~/Tinbo/plugins/ ; done
 echo "Copied plugins into tinbo directory."
