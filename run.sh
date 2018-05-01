522ab3dcc01af4a1c9644eb8a51605a6b30b7a15
Artur Bosch <arturbosch@gmx.de>
2018-05-01 12:26:56 +0200

    Update versions

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Djava.awt.headless=false tinbo-platform/build/libs/tinbo-platform-1.0.0.jar
+java -jar -Djava.awt.headless=false tinbo-platform/build/libs/tinbo-platform-1.0.1.jar
