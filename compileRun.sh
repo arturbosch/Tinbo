d76c373be226034355f7b1c71bb77ed3ee7d442a
Artur Bosch <arturbosch@gmx.de>
2018-05-08 20:46:31 +0200

    Update scripts

--- compileRun.sh
+++ compileRun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./gradlew build
-java -jar -Djava.awt.headless=false tinbo-platform/build/libs/tinbo-platform-1.0.0.jar
+java -jar -Djava.awt.headless=false tinbo-platform/build/libs/tinbo-platform-1.0.1.jar
